86363c63dc3718159515f3e7cadd2c195561f08b
SplinterSword <sparshj2003@gmail.com>
2026-08-29 18:51:21 +0530

    feat: enabled the keymaps in visual mode

--- lua/gitmarks/keymaps.lua
+++ lua/gitmarks/keymaps.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.createFileBinding(gitmarks, number)
-	vim.keymap.set("n", "<leader>g" .. number - 1, function()
+	vim.keymap.set({ "n", "v" }, "<leader>g" .. number - 1, function()
 		gitmarks.openMarkedFile(number)
 	end, {
 		desc = "Open Marked file " .. number - 1,
@@ -9,16 +9,16 @@ function M.createFileBinding(gitmarks, number)
 end
 
 function M.setup(gitmarks)
-	vim.keymap.set("n", "<leader>g", "<Nop>", {
+	vim.keymap.set({ "n", "v" }, "<leader>g", "<Nop>", {
 		desc = "[G]itmarks",
 	})
 
-	vim.keymap.set("n", "<leader>gm", "<Nop>", {
+	vim.keymap.set({ "n", "v" }, "<leader>gm", "<Nop>", {
 		desc = "[M]ark Current File",
 	})
 
 	for i = 1, 10 do
-		vim.keymap.set("n", "<leader>gm" .. i - 1, function()
+		vim.keymap.set({ "n", "v" }, "<leader>gm" .. i - 1, function()
 			gitmarks.markFile(i)
 		end, {
 			desc = "Mark file to " .. i - 1,
